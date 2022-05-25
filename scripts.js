572378d9f7a284b08fe3d078a24568425fb6687a
JovemAlex <alexsandrolima991@hotmail.com>
2022-05-25 17:36:37 -0300

    primeiro requisito

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-a
+const pixels = document.querySelectorAll('.pixel');
+const pixelBoard = document.querySelector('#pixel-board');
+const white = 'white';
+const black = 'black';
+const blue = 'blue';
+const green = 'green';
+const yellow = 'yellow';
+
+// window.onload = loadPage;
+
+function loadPage() {
+  for (let i in pixels) {
+    if (pixels[i].style.background !== white) {
+      pixels[i].style.background = white;
+    }
+  }
+}
