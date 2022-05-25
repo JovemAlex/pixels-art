c130b53079d0204a8d7a7561c75d976d90596d3e
JovemAlex <alexsandrolima991@hotmail.com>
2022-05-25 17:59:13 -0300

    atualização dos requisitos

--- scripts.js
+++ scripts.js
@@ -6,12 +6,3 @@ const blue = 'blue';
 const green = 'green';
 const yellow = 'yellow';
 
-// window.onload = loadPage;
-
-function loadPage() {
-  for (let i in pixels) {
-    if (pixels[i].style.background !== white) {
-      pixels[i].style.background = white;
-    }
-  }
-}
