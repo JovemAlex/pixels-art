21c665a6bbc7646a159e9cbc95a91a868d932a3c
JovemAlex <alexsandrolima991@hotmail.com>
2022-05-25 19:22:17 -0300

    requisitos 6 e 7

--- scripts.js
+++ scripts.js
@@ -6,3 +6,44 @@ const blue = 'blue';
 const green = 'green';
 const yellow = 'yellow';
 
+
+function loadPage() {
+  let caminhoPreto = document.querySelector('#black');
+  caminhoPreto.className = 'color selected';
+}
+
+const blackSelected = document.querySelector('#black');
+const blueSelected = document.querySelector('#blue');
+const yellowSelected = document.querySelector('#yellow');
+const greenSelected = document.querySelector('#green');
+
+blackSelected.addEventListener('click', () => {
+  blackSelected.classList.add('selected');
+  blueSelected.classList.remove('selected');
+  yellowSelected.classList.remove('selected');
+  greenSelected.classList.remove('selected');
+
+});
+blueSelected.addEventListener('click', () => {
+  blackSelected.classList.remove('selected');
+  blueSelected.classList.add('selected');
+  yellowSelected.classList.remove('selected');
+  greenSelected.classList.remove('selected');
+
+});
+yellowSelected.addEventListener('click', () => {
+  blackSelected.classList.remove('selected');
+  blueSelected.classList.remove('selected');
+  yellowSelected.classList.add('selected');
+  greenSelected.classList.remove('selected');
+
+});
+greenSelected.addEventListener('click', () => {
+  blackSelected.classList.remove('selected');
+  blueSelected.classList.remove('selected');
+  yellowSelected.classList.remove('selected');
+  greenSelected.classList.add('selected');
+
+});
+
+window.addEventListener('load', loadPage);
